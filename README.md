5a8e3f6af0134345366a566a2494f67f7ce3e532
Ari Firmanto <arifirmanto@gmail.com>
2021-10-16 14:02:35 +0700

    adding mvn build plugins

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ This is my own version of pokedex built using Java. I built this to participate 
 The theme of this jam is webcrawler, thus I crawl the pokemon data from https://bulbapedia.bulbagarden.net.
 
 ### How To Run
+```
+mvn package
+cd target
+run jar file with-dependencies
+```
+
+[![Watch the video](https://i9.ytimg.com/vi/6uS83V_Zryc/mq1.jpg?sqp=CPDwqYsG&rs=AOn4CLCtlvqC0NeyXUgIjzOdbTwH-nalJA)](https://youtu.be/6uS83V_Zryc)
--- src/main/java/javaJam/jPokedex/Main.java
+++ src/main/java/javaJam/jPokedex/Main.java
@@ -1,6 +1,5 @@
 package javaJam.jPokedex;
 
-
 import java.util.Scanner;
 
 public class Main {
@@ -8,31 +7,32 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
 
-//        SplashScreen sc = new SplashScreen(120, 20);
-//        sc.draw();
-//        Scanner console = new Scanner(System.in);
-//        char choice = 'n';
-//        PokeDex pokedex = new PokeDex("pokemon.txt");
+       SplashScreen sc = new SplashScreen(120, 20);
+       sc.draw();
+       Scanner console = new Scanner(System.in);
+       char choice = 'n';
+       PokeDex pokedex = new PokeDex("pokemon.txt");
 
-        System.out.println((int) (3.1756 * 1000));
 
-//        do {
-//            // Get the value from the user.
-//            System.out.println("");
-//            System.out.println("Please type pokemon name that you want to search: ");
-//
-//            String searchString = console.next();
-//            if (pokedex.getDatabases().contains(searchString)) {
-//                pokedex.setSearch(searchString);
-//                pokedex.analyze();
-//                pokedex.print();
-//                System.out.println("");
-//                System.out.println("Do you want to exit? Y or N: ");
-//                choice = console.next().charAt(0);
-//            } else {
-//                System.out.println(ConsoleColors.RED_BRIGHT + "Whoops, it seems the pokemon not exist!!" + ConsoleColors.RESET);
-//            }
-//        }
-//        while (choice == 'n' || choice == 'N');
+       do {
+           // Get the value from the user.
+           System.out.println("");
+           System.out.println("Please type pokemon name that you want to search: ");
+
+           String searchString = console.next();
+           if (pokedex.getDatabases().contains(searchString)) {
+               pokedex.setSearch(searchString);
+               pokedex.analyze();
+               pokedex.print();
+               System.out.println("");
+               System.out.println("Do you want to exit? Y or N: ");
+               choice = console.next().charAt(0);
+           } else {
+               System.out.println(ConsoleColors.RED_BRIGHT + "Whoops, it seems the pokemon not exist!!" + ConsoleColors.RESET);
+           }
+       }
+       while (choice == 'n' || choice == 'N');
+
+       console.close();
     }
 }
